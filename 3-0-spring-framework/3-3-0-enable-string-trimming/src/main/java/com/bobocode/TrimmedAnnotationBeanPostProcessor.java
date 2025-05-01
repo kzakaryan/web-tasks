9895d4f7f18db50a40f95c12074e165d0d4a3088
kzakaryan <kzakaryan@griddynamics.com>
2025-05-01 22:53:31 +0400

    Task 3-3-0

--- 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/TrimmedAnnotationBeanPostProcessor.java
+++ 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/TrimmedAnnotationBeanPostProcessor.java
@@ -2,20 +2,42 @@ package com.bobocode;
 
 import com.bobocode.annotation.Trimmed;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Method;
+
 /**
- * This is processor class implements {@link BeanPostProcessor}, looks for a beans where method parameters are marked with
- * {@link Trimmed} annotation, creates proxy of them, overrides methods and trims all {@link String} arguments marked with
- * {@link Trimmed}. For example if there is a string " Java   " as an input parameter it has to be automatically trimmed to "Java"
- * if parameter is marked with {@link Trimmed} annotation.
- * <p>
- *
- * Note! This bean is not marked as a {@link Component} to avoid automatic scanning, instead it should be created in
- * {@link StringTrimmingConfiguration} class which can be imported to a {@link Configuration} class by annotation
- * {@link EnableStringTrimming}
+ * This processor looks for beans where method parameters are marked with {@link Trimmed} annotation,
+ * creates a proxy of them, and trims all {@link String} arguments marked with {@link Trimmed}.
  */
-public class TrimmedAnnotationBeanPostProcessor {
-//todo: Implement TrimmedAnnotationBeanPostProcessor according to javadoc
+public class TrimmedAnnotationBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        // Not necessary to modify this method for our functionality.
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        // If the bean has methods with parameters annotated with @Trimmed, we will create a proxy
+        return Enhancer.create(bean.getClass(), new MethodInterceptor() {
+            @Override
+            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                // Iterate through method parameters and trim all strings annotated with @Trimmed
+                if (args != null) {
+                    for (int i = 0; i < args.length; i++) {
+                        if (args[i] instanceof String && method.getParameters()[i].isAnnotationPresent(Trimmed.class)) {
+                            args[i] = ((String) args[i]).trim(); // Trim the string
+                        }
+                    }
+                }
+                // Call the original method
+                return proxy.invokeSuper(obj, args);
+            }
+        });
+    }
 }
--- 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/annotation/Trimmed.java
+++ 3-0-spring-framework/3-3-0-enable-string-trimming/src/main/java/com/bobocode/annotation/Trimmed.java
@@ -6,10 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation that can be placed on {@link  String} method parameters to enable automatic trimming values of these parameters.
+ * Annotation that can be placed on {@link String} method parameters to enable automatic trimming values of these parameters.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface Trimmed {
-
 }
